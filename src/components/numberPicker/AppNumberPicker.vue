7cb510660bbaa17b9d4a3a82485a913a3636670e
ziaadini <y.ziaadini@gmail.com>
2020-12-30 18:22:17 +0330

    fix button size

--- src/components/numberPicker/AppNumberPicker.vue
+++ src/components/numberPicker/AppNumberPicker.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="flex items-center">
-    <app-button @click="plus" v-bind="buttonProps">
+    <app-button @click="plus" class="w-10" v-bind="buttonProps">
       +
     </app-button>
     <span class="w-10 text-center">{{ modelValue }}</span>
-    <app-button @click="minus" v-bind="buttonProps">
+    <app-button @click="minus" class="w-10" v-bind="buttonProps">
       -
     </app-button>
   </div>
