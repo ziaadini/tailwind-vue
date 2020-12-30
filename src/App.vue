ffb99099ecfa55adc1dbd8f8ac3f47e3db13bdab
ali <alizangiabadicode@gmail.com>
2020-12-30 12:01:05 +0330

    app button added

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,35 @@
 <template>
   <div class="flex justify-center">
-     <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" src="./assets/logo.png" />
   </div>
   <div class="flex items-center flex-col ">
-    <AppButton variant="warning" rounded/>
-      <app-number-picker :max="5" v-model="numberPickerCount"></app-number-picker>
+    <AppButton variant="warning" rounded icon="accessible" disabled @click="clicked" />
+    <app-number-picker :max="5" v-model="numberPickerCount"></app-number-picker>
     <div>#zia</div>
     <div></div>
   </div>
-
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 import AppNumberPicker from "@/components/AppNumberPicker.vue";
 import AppButton from "@/components/AppButton.vue";
-import {ref} from 'vue'
+import { ref } from "vue";
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
     AppNumberPicker,
-    AppButton
+    AppButton,
   },
-  setup(){
-    const  numberPickerCount=ref(3)
+  setup() {
+    const numberPickerCount = ref(3);
 
-    return {numberPickerCount}
-  }
+    return { numberPickerCount };
+  },
+  methods: {
+    clicked(): void {
+      console.log("btn is clicked");
+    },
+  },
 });
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
@@ -9,21 +9,21 @@ module.exports = {
           light: colors.blue,
           DEFAULT: "#377DFF",
           dark: "#124198",
-          hover:'#658dd7'
+          hover: "#658dd7",
         },
         success: {
           light: "#8be77b",
           DEFAULT: "#00bd1d",
           dark: "#008916",
-          hover: "#6cc7a7"
+          hover: "#6cc7a7",
         },
-        danger:{
+        danger: {
           light: "#f16484",
           DEFAULT: "#f53f66",
           dark: "#a80029",
           hover: "#ef7491",
         },
-        warning:{
+        warning: {
           light: "#ec9f76",
           DEFAULT: "#e88550",
           dark: "#f19564",
@@ -31,29 +31,31 @@ module.exports = {
         },
       },
       fontSize: {
-        'xs': '12px',
-        'sm': '14px',
-        'base': '16px',
-        'lg': '18px',
-        'xl': '20px',
-        '2xl': '22px',
-        '3xl': '24px',
-        '4xl': '26px',
-        '5xl': '28px',
+        xs: "12px",
+        sm: "14px",
+        base: "16px",
+        lg: "18px",
+        xl: "20px",
+        "2xl": "22px",
+        "3xl": "24px",
+        "4xl": "26px",
+        "5xl": "28px",
       },
       borderRadius: {
-        'none': '0',
-        'sm': '8px',
-        DEFAULT: '16px',
-        'md': '20px',
-        'lg': '26px',
-        'large': '32px',
-        'full': '9999px',
-      }
+        none: "0",
+        sm: "8px",
+        DEFAULT: "16px",
+        md: "20px",
+        lg: "26px",
+        large: "32px",
+        full: "9999px",
+      },
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ["disabled"],
+    },
   },
   plugins: [],
-}
+};
