ee0e84761dd94e78a7f8e8c0333386144d199eaf
ziaadini <y.ziaadini@gmail.com>
2020-12-31 12:56:44 +0330

    modal has been completed

--- src/compositionFunctions/keyboardEvents.ts
+++ src/compositionFunctions/keyboardEvents.ts
@@ -1,4 +1,9 @@
-import {onMounted,onUnmounted} from 'vue'
-export default function(callback){
-
-}
+import { onMounted, onUnmounted } from "vue";
+export const useKeyDown = (callback: () => void) => {
+  onMounted(() => {
+    document.addEventListener("keydown", callback);
+  });
+  onUnmounted(() => {
+    document.removeEventListener("keydown", callback);
+  });
+};
