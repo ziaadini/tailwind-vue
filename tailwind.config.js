1675e708c390a0e5a0a77c02d5c39f705f03137f
ali <alizangiabadicode@gmail.com>
2021-01-04 08:42:52 +0330

    whitelist added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 const colors = require("tailwindcss/colors");
 module.exports = {
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  purge: {
+    content: ['./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}'],
+
+    // These options are passed through directly to PurgeCSS
+    options: {
+      safelist: {
+        standard: [/primary/, /warning/, /danger/, /success/],
+      },
+    },
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
