e268eabec9bf227cf2fd01e5e4daac1d1e865fd8
ali <alizangiabadicode@gmail.com>
2021-01-05 17:09:41 +0330

    whitelist problem solved

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,17 @@
 const colors = require("tailwindcss/colors");
 module.exports = {
-  purge: ['./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}'],
-  // purge: {
-  //   content: ['./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}'],
-
-  //   // These options are passed through directly to PurgeCSS
-  //   // options: {
-  //   //   safelist: {
-  //   //     standard: [],
-  //   //     keyframes: true,
-  //   //     fontFace: true,
-  //   //   },
-  //   // },
-  // },
+  purge: {
+    content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+    enable: true,
+    // These options are passed through directly to PurgeCSS
+    options: {
+      safelist: {
+        standard: [/primary/, /danger/, /warning/, /success/, ],
+        keyframes: true,
+        fontFace: true,
+      },
+    },
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
