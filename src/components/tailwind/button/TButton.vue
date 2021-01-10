b2a150661cb1aabd07d7d67fd74e4df71fb190cd
ali <alizangiabadicode@gmail.com>
2021-01-10 18:05:23 +0330

    removing hover text white replacing ring with border adding -50 to outline hover

--- src/components/tailwind/button/TButton.vue
+++ src/components/tailwind/button/TButton.vue
@@ -7,7 +7,7 @@
       ripple,
       'w-full': full,
     }"
-    class="shadow relative rounded-sm block border-1 align-se focus:outline-none px-4 py-2 hover:text-white flex justify-center align-center "
+    class="shadow relative rounded-sm block border-1 align-se focus:outline-none px-4 py-2  flex justify-center align-center "
     v-bind="$attrs"
   >
     <div class="contents">
@@ -88,14 +88,13 @@ export default defineComponent({
       const baseClass =
         " hover:opacity-80 transition text-white disabled:opacity-50";
       const outlineBaseClass =
-        " transition ring-4 text-dark  hover:opacity-80 disabled:opacity-50";
-      return (
-        (props.outline
-          ? `bg-white ring-${props.variant} hover:bg-${props.variant}`
-          : `
-        bg-${props.variant} ring-${props.variant} hover:bg-${props.variant}
-        `) + (props.outline ? outlineBaseClass : baseClass)
-      );
+        " transition border-4 text-dark  hover:opacity-80 disabled:opacity-50";
+      return props.outline
+        ? `bg-white border-${props.variant} hover:bg-${props.variant}-50` +
+            outlineBaseClass
+        : `
+        bg-${props.variant} hover:bg-${props.variant}
+        ` + baseClass;
     });
 
     return {
