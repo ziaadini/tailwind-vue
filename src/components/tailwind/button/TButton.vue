b2cc6535822b5d0874abfb98dc1f550b25666cc7
ali <alizangiabadicode@gmail.com>
2021-01-10 17:46:54 +0330

    adding loading to button

--- src/components/tailwind/button/TButton.vue
+++ src/components/tailwind/button/TButton.vue
@@ -5,21 +5,41 @@
       'rounded-full': rounded,
       [variantClasses]: true,
       ripple,
-      'w-full': full
+      'w-full': full,
     }"
-    class="shadow rounded-sm block border-1 focus:outline-none px-4 py-2 hover:text-white flex justify-center align-center "
+    class="shadow relative rounded-sm block border-1 align-se focus:outline-none px-4 py-2 hover:text-white flex justify-center align-center "
     v-bind="$attrs"
   >
-    <app-icon :name="icon" />
-    <span>
-      <slot />
-    </span>
+    <div class="contents">
+      <app-icon
+        :name="icon"
+        :class="{
+          'opacity-0': loading,
+        }"
+      />
+      <span
+        :class="{
+          'opacity-0': loading,
+        }"
+      >
+        <slot />
+      </span>
+    </div>
+    <div
+      v-if="loading"
+      :class="{
+        'absolute transform top-1/2 -translate-y-1/2': loading,
+      }"
+    >
+      <t-loading size="sm" />
+    </div>
   </button>
 </template>
 <script lang="ts">
 import { computed, defineComponent } from "vue";
 import AppIcon from "@/components/icon/TIcon.vue";
 import { variants } from "@/utility/css-helper.ts";
+import TLoading from "@/components/tailwind/loading/TLoading.vue";
 export default defineComponent({
   name: "TButton",
   props: {
@@ -51,11 +71,17 @@ export default defineComponent({
     full: {
       type: Boolean,
       default: false,
-      required: false
-    }
+      required: false,
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
   },
   components: {
     AppIcon,
+    TLoading,
   },
   setup(props) {
     const variantClasses = computed((): string => {
