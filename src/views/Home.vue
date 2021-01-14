b8adc3bad1e8d40a1c1f1ecf937e18caf417a730
ali <alizangiabadicode@gmail.com>
2021-01-14 08:54:44 +0330

    some changes to home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,45 +1,30 @@
 <template>
-  <div class="flex justify-center">
-    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <div class="flex justify-center flex-wrap space-x-1 space-x-reverse">
+    <t-button variant="primary">Test</t-button>
+    <t-button ripple variant="warning">Test</t-button>
+    <t-button rounded variant="danger">Test</t-button>
+    <t-button rounded variant="success">Test</t-button>
   </div>
-  <div class="flex items-center flex-col space-y-4">
-    <div>
-      <div>#Ali</div>
-      <t-button variant="warning" icon="accessible" outline disabled>
-        some text
-      </t-button>
-    </div>
-    <div>
-      <div>#zia</div>
-      <t-number-picker :max="5" v-model="numberPickerCount"></t-number-picker>
-    </div>
-    <div>
-      <div>#zia</div>
-      <t-modal v-model="modal" :close-callback="modalCloseCallback">
-        <!--        <template #closeButton="attrs">-->
-        <!--          <t-button v-bind="attrs">close</t-button>-->
-        <!--        </template>-->
-        <p class="text-gray-800">
-          Are you sure you want you delete your account? This action cannot be
-          undone.
-        </p>
-
-        <div class="text-right mt-4">
-          <button
-            @click="modal = false"
-            class="px-4 py-2 text-sm text-gray-600 focus:outline-none hover:underline"
-          >
-            Cancel
-          </button>
-          <button
-            class="mr-2 px-4 py-2 text-sm rounded text-white bg-red-500 focus:outline-none hover:bg-red-400"
-          >
-            Delete
-          </button>
-        </div>
-      </t-modal>
-      <t-button @click="modal = true">open modal</t-button>
-    </div>
+  <br />
+  <div class="flex justify-center flex-wrap space-x-1 space-x-reverse">
+    <t-button outline variant="primary">Test</t-button>
+    <t-button outline ripple variant="warning">Test</t-button>
+    <t-button rounded outline variant="danger">Test</t-button>
+    <t-button rounded outline variant="success">Test</t-button>
+  </div>
+  <br />
+  <div class="flex justify-center flex-wrap space-x-1 space-x-reverse">
+    <t-button loading variant="primary">Test</t-button>
+    <t-button loading ripple variant="warning">Test</t-button>
+    <t-button loading rounded variant="danger">Test</t-button>
+    <t-button loading rounded variant="success">Test</t-button>
+  </div>
+  <br />
+  <div class="flex justify-center flex-wrap space-x-1 space-x-reverse">
+    <t-button loading outline variant="primary">Test</t-button>
+    <t-button loading outline ripple variant="warning">Test</t-button>
+    <t-button loading rounded outline variant="danger">Test</t-button>
+    <t-button loading rounded outline variant="success">Test</t-button>
   </div>
 </template>
 
@@ -47,14 +32,10 @@
 import { defineComponent } from "vue";
 import { ref } from "vue";
 import TButton from "@/components/tailwind/button/TButton.vue";
-import TNumberPicker from "@/components/tailwind/numberPicker/TNumberPicker.vue";
-import TModal from "@/components/tailwind/modal/TModal.vue";
 export default defineComponent({
   name: "App",
   components: {
-    TModal,
-    TNumberPicker,
-    TButton
+    TButton,
   },
   setup() {
     const numberPickerCount = ref<number>(3);
@@ -64,6 +45,6 @@ export default defineComponent({
       return true;
     };
     return { numberPickerCount, modal, modalCloseCallback };
-  }
+  },
 });
 </script>
