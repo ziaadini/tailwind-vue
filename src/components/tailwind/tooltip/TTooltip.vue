0bb1dbb36356d34e8b5db52648e7242a6b3b6cad
ziaadini <y.ziaadini@gmail.com>
2021-01-20 16:32:37 +0330

    package.json remove tooltip extention

--- src/components/tailwind/tooltip/TTooltip.vue
+++ src/components/tailwind/tooltip/TTooltip.vue
@@ -4,16 +4,16 @@
     <div
       class="absolute right-1/2 transform translate-x-1/2 w-full text-gray-50"
     >
-      <div
-        class="w-0 h-0 mx-auto border-l-8 border-b-8 border-r-8 border-l-transparent border-r-transparent  border-primary"
-      ></div>
+      <t-triangle class="mx-auto"></t-triangle>
       <div class="bg-primary rounded-xs p-1 w-full">tooltip</div>
     </div>
   </div>
 </template>
 
 <script>
+import TTriangle from "@/components/tailwind/triangle/TTriangle";
 export default {
-  name: "TTooltip"
+  name: "TTooltip",
+  components: { TTriangle }
 };
 </script>
---
+++ src/components/tailwind/triangle/TTriangle.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="w-0 h-0 border-primary" :class="[triangleClassByDirection]"></div>
+</template>
+
+<script>
+import { computed } from "vue";
+
+export default {
+  name: "TTriangle",
+  props: {
+    direction: {
+      type: String,
+      validator: value => {
+        return ["arrow-up", "arrow-down", "arrow-right", "arrow-left"].includes(
+          value
+        );
+      },
+      default: "arrow-up"
+    }
+  },
+  setup(props) {
+    const triangleClassByDirection = computed(() => {
+      switch (props.direction) {
+        case "arrow-up":
+          return "border-l-8 border-b-8 border-r-8 border-l-transparent border-r-transparent";
+        case "arrow-down":
+          return "border-l-8 border-b-8 border-r-8 border-l-transparent border-r-transparent";
+      }
+    });
+    return {
+      triangleClassByDirection
+    }
+  }
+};
+</script>
+
+<style scoped></style>
