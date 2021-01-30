01a4a31313fb426c5bfa87cd7cf869ad125ee964
ziaadini <y.ziaadini@gmail.com>
2021-01-30 10:44:11 +0330

    component lists

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,45 @@
-import TAccordion from "./components/tailwind/alert/TAlert.vue";
-export { TAccordion };
+import TAccordion from "./components/tailwind/accoirdion/TAccordion.vue";
+import TAlert from "./components/tailwind/alert/TAlert.vue";
+import TBottomSheet from "./components/tailwind/bottomSheet/TBottomSheet.vue";
+import TBreadcrumb from "./components/tailwind/breadcrumb/TBreadcrumb.vue";
+import TButton from "./components/tailwind/button/TButton.vue";
+import TCard from "./components/tailwind/card/TCard.vue";
+import TCheckbox from "./components/tailwind/checkbox/TCheckbox.vue";
+import TDrawer from "./components/tailwind/drawer/TDrawer.vue";
+import TDropdown from "./components/tailwind/dropdown/TDropdown.vue";
+import TLoading from "./components/tailwind/loading/TLoading.vue";
+import TMenu from "./components/tailwind/menu/TMenu.vue";
+import TModal from "./components/tailwind/modal/TModal.vue";
+import TNumberPicker from "./components/tailwind/numberPicker/TNumberPicker.vue";
+import TPagination from "./components/tailwind/paginantion/TPagination.vue";
+import TProgressBar from "./components/tailwind/progress/TProgressBar.vue";
+import TRadio from "./components/tailwind/radio/TRadio.vue";
+import TSkeleton from "./components/tailwind/skeleton/TSkeleton.vue";
+import TStepper from "./components/tailwind/stepper/TStepper.vue";
+import TSwitch from "./components/tailwind/switch/TSwitch.vue";
+import TTabs from "./components/tailwind/tab/TTabs.vue";
+import TTabItem from "./components/tailwind/tab/TTabItem.vue";
+
+export {
+  TAlert,
+  TAccordion,
+  TBottomSheet,
+  TBreadcrumb,
+  TButton,
+  TCard,
+  TCheckbox,
+  TDrawer,
+  TDropdown,
+  TLoading,
+  TMenu,
+  TModal,
+  TNumberPicker,
+  TPagination,
+  TProgressBar,
+  TRadio,
+  TSkeleton,
+  TStepper,
+  TSwitch,
+  TTabs,
+  TTabItem
+};
