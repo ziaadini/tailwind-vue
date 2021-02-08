59cd9140473e97584239354de92b468bbd386d70
ali <alizangiabadicode@gmail.com>
2021-02-08 12:03:26 +0330

    adding custom template to is scroll composition

--- src/compositionFunctions/isScrolling.ts
+++ src/compositionFunctions/isScrolling.ts
@@ -1,20 +1,24 @@
 import { onUnmounted, ref } from "vue";
 import { onMounted } from "vue";
-export const useIsScrolling = (onMount = false) => {
+export const useIsScrolling = (onMount = false, element = ref(null)) => {
   const result = ref({});
   const handleScroll = (e) => {
     result.value = e;
   };
   const initiateScroll = () => {
-    console.log('scroll event initiate')
-    document.addEventListener("scroll", handleScroll);
+    console.log("scroll event initiate", element);
+    if (element.value) {
+      (element.value! as HTMLElement).addEventListener("scroll", handleScroll);
+    } else document.addEventListener("scroll", handleScroll);
   };
   const destroyScroll = () => {
-    console.log('scroll event destroyed')
-    document.removeEventListener("scroll", handleScroll);
+    console.log("scroll event destroyed", element);
+    if (element.value) {
+      (element.value! as HTMLElement).removeEventListener("scroll", handleScroll);
+    } else document.removeEventListener("scroll", handleScroll);
   };
   onMounted(() => {
-    if (onMount) document.addEventListener("scroll", handleScroll);
+    if (onMount) initiateScroll()
   });
 
   onUnmounted(() => {
@@ -24,6 +28,7 @@ export const useIsScrolling = (onMount = false) => {
   return {
     result,
     destroyScroll,
-    initiateScroll
+    initiateScroll,
+    element,
   };
 };
