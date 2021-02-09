9e0a7fafeaeb5cd47f37e069f7c02f130f1a7393
ali <alizangiabadicode@gmail.com>
2021-02-09 10:12:02 +0330

    image downloader composition function added

---
+++ src/compositionFunctions/image.ts
@@ -0,0 +1,24 @@
+import { ref } from "vue";
+export const useImageDownloader = () => {
+  const image = ref(null as any);
+  const downloadingImage = new Image();
+  const downloadImage = (src) => {
+    downloadingImage.onload = function() {
+      // @ts-ignore
+      image.value = src;
+    };
+    downloadingImage.src = src;
+  };
+
+
+  const setImage = (imageRef: any) => {
+    console.log(imageRef, image)
+    imageRef.value.src = image.value;
+  };
+
+  return {
+    image,
+    setImage,
+    downloadImage
+  };
+};
