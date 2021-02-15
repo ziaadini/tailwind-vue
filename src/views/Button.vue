a7400d7bc7ed45a391b8ff0cf83eedcf410f705c
mohstarsos1 <mohammad.ghasemi68@gmail.com>
2021-02-15 19:37:49 +0330

    add some button page2

--- src/views/Button.vue
+++ src/views/Button.vue
@@ -3,14 +3,14 @@
     <h3 class="text-5xl font-bold mb-2">Button</h3>
     <p class="mb-2">Small filled buttons in different styles and colors can be used for call to actions in forms and more.</p>
 
-    <div class="flex flex-col items-start mt-10 border rounded-sm p-3 w-full">
+    <div class="flex flex-col items-start mt-10 border rounded-sm p-3 w-full shadow-md">
       <h2 class="text-xl font-bold mb-2">Simple (Only Text)</h2>
       <p class="mb-2">Choose this basic button, only with text.</p>
       <t-button class="mb-2">Simple</t-button>
       <iframe width="100%" height="100" src="//jsfiddle.net/mohstarsos1/13xgw0ry/8/embedded/html/dark" allowfullscreen="allowfullscreen" allowpaymentrequest frameborder="0"></iframe>
     </div>
 
-    <div class="flex flex-col items-start mt-10 border rounded-sm p-3 w-full">
+    <div class="flex flex-col items-start mt-10 border rounded-sm p-3 w-full shadow-md">
       <h2 class="text-xl font-bold mb-2">Simple (with ripple and variant colors)</h2>
       <p class="mb-2">Choose this basic button, with ripple and default variant colors.</p>
       <div class="mb-2 flex">
@@ -22,7 +22,7 @@
       <iframe width="100%" height="160" src="//jsfiddle.net/mohstarsos1/c925pg8s/1/embedded/html/dark/" allowfullscreen="allowfullscreen" allowpaymentrequest frameborder="0"></iframe>
     </div>
 
-    <div class="flex flex-col items-start mt-10 border rounded-sm p-3 w-full">
+    <div class="flex flex-col items-start mt-10 border rounded-sm p-3 w-full shadow-md">
       <h2 class="text-xl font-bold mb-2">Outline, rounded and disabled</h2>
       <p class="mb-2">Choose this basic button, with outline, rounded and disabled.</p>
       <div class="mb-2 flex">
@@ -46,7 +46,7 @@
       <iframe width="100%" height="300" src="//jsfiddle.net/mohstarsos1/ftdboukw/4/embedded/html/dark/" allowfullscreen="allowfullscreen" allowpaymentrequest frameborder="0"></iframe>
     </div>
 
-    <div class="flex flex-col items-start mt-10 border rounded-sm p-3 w-full">
+    <div class="flex flex-col items-start mt-10 border rounded-sm p-3 w-full shadow-md">
       <h2 class="text-xl font-bold mb-2">With icon and text or icon only</h2>
       <p class="mb-2">Choose this basic button, with icon and text or icon only.</p>
       <div class="mb-2 flex">
@@ -64,6 +64,31 @@
         <t-button variant="danger" icon="delete_outline" ripple class="mr-2"></t-button>
       </div>
       <iframe width="100%" height="280" src="//jsfiddle.net/mohstarsos1/vek8fz1o/2/embedded/html/dark/" allowfullscreen="allowfullscreen" allowpaymentrequest frameborder="0"></iframe>
+    </div>
+
+    <div class="flex flex-col items-start mt-10 border rounded-sm p-3 w-full shadow-md">
+      <h2 class="text-xl font-bold mb-2">Loading</h2>
+      <p class="mb-2">Choose this basic button, with loading icon.</p>
+      <div class="mb-2 flex">
+        <t-button loading variant="primary" outline class="mr-2">.....</t-button>
+        <t-button loading variant="primary" class="mr-2">.....</t-button>
+        <t-button loading variant="success" rounded class="mr-2">...</t-button>
+        <t-button loading variant="warning" disabled class="mr-2">.....</t-button>
+        <t-button loading variant="danger" class="mr-2">.....</t-button>
+      </div>
+      <iframe width="100%" height="150" src="//jsfiddle.net/mohstarsos1/c925pg8s/1/embedded/html/dark/" allowfullscreen="allowfullscreen" allowpaymentrequest frameborder="0"></iframe>
+    </div>
+
+    <div class="flex flex-col items-start mt-10 border rounded-sm p-3 w-full shadow-md">
+      <h2 class="text-xl font-bold mb-2">Block (full-width size)</h2>
+      <p class="mb-2">Choose this basic button, with block full width of screen.</p>
+      <div class="mb-2">
+        <t-button variant="primary" full ripple class="mb-2">Primary</t-button>
+        <t-button variant="success" full ripple class="mb-2">Success</t-button>
+        <t-button variant="warning" full ripple class="mb-2">Warning</t-button>
+        <t-button variant="danger" full ripple>Danger</t-button>
+      </div>
+      <iframe width="100%" height="150" src="//jsfiddle.net/mohstarsos1/ymr0czbf/1/embedded/html/dark/" allowfullscreen="allowfullscreen" allowpaymentrequest frameborder="0"></iframe>
     </div>
   </div>
 </template>
