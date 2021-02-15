7e2cee413b718501ffa32724182f647de48553b8
mohstarsos1 <mohammad.ghasemi68@gmail.com>
2021-02-15 16:37:06 +0330

    initial doc

--- src/router/index.ts
+++ src/router/index.ts
@@ -31,6 +31,14 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "zia" */ "@/views/Zia.vue")
+  },
+  {
+    path: "/button",
+    name: "button",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "zia" */ "@/views/Button.vue")
   }
 ];
 
---
+++ src/views/Button.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex flex-col items-start" dir="ltr">
+    <h3 class="text-5xl font-bold mb-2">Button</h3>
+    <p class="mb-2">Small filled buttons in different styles and colors can be used for call to actions in forms and more.</p>
+
+    <div class="flex flex-col items-start mt-10">
+      <h2 class="text-xl font-bold mb-2">Simple (Only Text)</h2>
+      <p class="mb-2">Choose this basic button, only with text.</p>
+      <t-button>Simple</t-button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import TButton from "@/components/tailwind/button/TButton.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    TButton,
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
