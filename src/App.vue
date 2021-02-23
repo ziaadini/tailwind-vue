9009aa083477286c8e23e936af3551d1496a9e98
mohstarsos1 <mohammad.ghasemi68@gmail.com>
2021-02-23 18:36:29 +0330

    add menu of components

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,47 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/ali">Ali</router-link> |
     <router-link to="/mohammad">mohammad</router-link> |
-    <router-link to="/zia">zia</router-link>
+    <router-link to="/zia">zia</router-link> |
+    <t-dropdown
+      v-model="dropdownModel"
+      hover
+      placeholder="Component's Document"
+      rounded
+      :items="ddItems"
+      variant="success"
+    />
   </div>
   <router-view />
 </template>
 <script lang="ts">
 import { defineComponent, provide } from "vue";
 import { TSettings } from "@/utility/types/TSettings";
+import TDropdown from "@/components/tailwind/dropdown/TDropdown.vue";
+
 export default defineComponent({
+  components: {
+    TDropdown,
+  },
+  data() {
+    return {
+      dropdownModel: "",
+      ddItems: ["button", "alert", "textInput"],
+    };
+  },
   setup() {
     provide("TSettings", {
       tabs: {
         header: {
           add: "hover:bg-gray-100 py-3",
           delete: ["py-4"],
-          replace: { "text-gray-600": "text-gray-700" }
-        }
+          replace: { "text-gray-600": "text-gray-700" },
+        },
       },
       card: {
         title: {
-          add: "text-danger"
-        }
-      }
+          add: "text-danger",
+        },
+      },
       // alert: {
       //   container: { add: "py-1" }
       // }
@@ -36,7 +55,7 @@ export default defineComponent({
     provide("t-table-localSort", false);
     provide("t-bottom-sheet-titleTag", "h5");
     // provide("t-table-loading", true);
-  }
+  },
 });
 </script>
 <style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -48,6 +48,14 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "zia" */ "@/views/Alert.vue")
+  },
+  {
+    path: "/textinput",
+    name: "textinput",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "zia" */ "@/views/TextInput.vue")
   }
 ];
 
