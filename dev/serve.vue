2b22c0ec9de8f4790caaf58e6fa39e77d99e8805
ali <alizangiabadicode@gmail.com>
2021-03-17 14:09:04 +0330

    done

---
+++ dev/serve.vue
@@ -0,0 +1,32 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { TAlert } from "../dist/npm-library.esm";
+// import { TBadge } from "../dist/npm-library.esm";
+// import { TAccordion } from "../dist/npm-library.esm";
+import '../src/assets/css/index.css'
+export default defineComponent({
+  name: "ServeDev",
+  components: {
+    // TAlert,
+    // TBadge,
+    // TAccordion,
+  },
+  data() {
+    return {
+      show: false,
+    };
+  },
+});
+</script>
+
+<template>
+  <div id="app" class="bg-red-500 h-screen">
+      <TAlert show variant="primary" v-model="show">
+        sfd
+      </TAlert>
+    <t-badge>
+      <TAccordion :items="[{title: 'ali', text: 'ali'}]"></TAccordion>
+    </t-badge>
+  </div>
+</template>
---
+++ src/lib-components/index.ts
@@ -0,0 +1,7 @@
+// import TAlert from "../components/tailwind/alert/TAlert.vue";
+// import TAccordion from "../components/tailwind/accoirdion/TAccordion.vue";
+// import TBadge from "../components/tailwind/badge/TBadge.vue";
+
+export { default as TAlert } from "../components/tailwind/alert/TAlert.vue";
+export { default as TAccordion } from "../components/tailwind/accoirdion/TAccordion.vue";
+export { default as TBadge } from "../components/tailwind/badge/TBadge.vue";
