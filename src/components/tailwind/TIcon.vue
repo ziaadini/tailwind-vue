a49f0b8d59b5d04584913a0ebcb35dc47f200fac
ali <alizangiabadicode@gmail.com>
2021-03-18 12:43:13 +0330

    add publish

--- src/components/tailwind/TIcon.vue
+++ src/components/tailwind/TIcon.vue
@@ -1,22 +1,43 @@
 <template>
-  <i v-if="name" class="material-icons pl-1" :class="{'opacity-40':disabled}">
-    {{ name }}
+  <i
+    v-if="name"
+    data-name="icon-wrapper"
+    :class="[
+      renderClass('material-icons pl-1', 'wrapper', { 'opacity-40': disabled }),
+    ]"
+  >
+    <template v-if="!avoidText">
+      {{ name }}
+    </template>
   </i>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { useRenderClass } from "@/compositionFunctions/settings";
+import { defineComponent, inject } from "vue";
+const component = (propName: string) => "t-icon-" + propName;
+
 export default defineComponent({
   name: "TIcon",
   props: {
     name: {
       type: String,
-      default: ""
+      default: "",
     },
     disabled: {
       type: Boolean,
-      default: () => false
-    }
-  }
+      default: () => false,
+    },
+    avoidText: {
+      type: Boolean,
+      default: () => inject(component("i"), false),
+    },
+  },
+  setup() {
+    const { renderClass } = useRenderClass("button");
+    return {
+      renderClass,
+    };
+  },
 });
 </script>
