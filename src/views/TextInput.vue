54e89fb6201d5ba7f4a002e88354b9933f3396c5
mohstarsos1 <mohammad.ghasemi68@gmail.com>
2021-03-30 18:44:31 +0430

    finish text input

--- src/views/TextInput.vue
+++ src/views/TextInput.vue
@@ -12,11 +12,11 @@
         Simple (Only Text Input with Placeholder)
       </h2>
       <p class="mb-2">Choose this basic text input, only with placeholder.</p>
-      <TTextInput placeholder="some placeholder" variant="white" class="mb-3" />
+      <TTextInput placeholder="some placeholder" class="mb-3" />
       <iframe
         width="100%"
         height="100"
-        src="//jsfiddle.net/mohstarsos1/hqsLy3bd/2/embedded/html/dark/"
+        src="//jsfiddle.net/mohstarsos1/hqsLy3bd/4/embedded/html/dark/"
         allowfullscreen="allowfullscreen"
         allowpaymentrequest
         frameborder="0"
@@ -27,24 +27,121 @@
       class="flex flex-col items-start mt-10 border rounded-sm p-3 w-full shadow-md"
     >
       <h2 class="text-xl font-bold mb-2">
-        Simple (with ripple and variant colors)
+        With Placeholder and icon
       </h2>
       <p class="mb-2">
-        Choose this basic button, with ripple and default variant colors.
+        Choose this basic text input, with Placeholder and icon. (you can change
+        alignment of icon)
       </p>
       <div class="mb-2 flex">
+        <TTextInput placeholder="some placeholder" right-icon="bookmarks" />
+      </div>
+      <div class="mb-2 flex">
+        <TTextInput placeholder="some placeholder" left-icon="bookmarks" />
+      </div>
+      <div class="mb-2 flex">
         <TTextInput
-          placeholder="some test placeholder"
-          v-model.format:,:4="myNumber"
-          inputType="numeric"
-          variant="primary"
+          placeholder="some placeholder"
           right-icon="bookmarks"
+          left-icon="speed"
         />
       </div>
       <iframe
         width="100%"
         height="160"
-        src="//jsfiddle.net/mohstarsos1/c925pg8s/1/embedded/html/dark/"
+        src="//jsfiddle.net/mohstarsos1/2neghr0j/4/embedded/html/dark/"
+        allowfullscreen="allowfullscreen"
+        allowpaymentrequest
+        frameborder="0"
+      ></iframe>
+    </div>
+
+    <div
+      class="flex flex-col items-start mt-10 border rounded-sm p-3 w-full shadow-md"
+    >
+      <h2 class="text-xl font-bold mb-2">
+        Varient color and disabled
+      </h2>
+      <p class="mb-2">
+        Choose this text input, with default variant colors and disabled mode.
+      </p>
+      <div class="mb-2 flex">
+        <TTextInput placeholder="some placeholder" variant="white" />
+      </div>
+      <div class="mb-2 flex">
+        <TTextInput placeholder="some placeholder" variant="primary" />
+      </div>
+      <div class="mb-2 flex">
+        <TTextInput placeholder="some placeholder" variant="success" />
+      </div>
+      <div class="mb-2 flex">
+        <TTextInput placeholder="some placeholder" variant="warning" />
+      </div>
+      <div class="mb-2 flex">
+        <TTextInput placeholder="some placeholder" variant="danger" />
+      </div>
+      <div class="mb-2 flex">
+        <TTextInput
+          placeholder="some test placeholder"
+          variant="success"
+          disabled
+        />
+      </div>
+      <iframe
+        width="100%"
+        height="200"
+        src="//jsfiddle.net/mohstarsos1/m4wnxfhq/4/embedded/html/dark/"
+        allowfullscreen="allowfullscreen"
+        allowpaymentrequest
+        frameborder="0"
+      ></iframe>
+    </div>
+
+    <div
+      class="flex flex-col items-start mt-10 border rounded-sm p-3 w-full shadow-md"
+    >
+      <h2 class="text-xl font-bold mb-2">
+        Rounded, Hover style and error handle border color
+      </h2>
+      <p class="mb-2">
+        Choose this text input, with Rounded, Hover style and error handle
+        border color.
+      </p>
+      <div class="mb-2 flex">
+        <TTextInput placeholder="some placeholder" rounded />
+      </div>
+      <div class="mb-2 flex">
+        <TTextInput placeholder="some placeholder" variant="success" hover />
+      </div>
+      <div class="mb-2 flex">
+        <TTextInput placeholder="some placeholder" error />
+      </div>
+      <iframe
+        width="100%"
+        height="150"
+        src="//jsfiddle.net/mohstarsos1/hcf4eL6s/1/embedded/html/dark/"
+        allowfullscreen="allowfullscreen"
+        allowpaymentrequest
+        frameborder="0"
+      ></iframe>
+    </div>
+
+    <div
+      class="flex flex-col items-start mt-10 border rounded-sm p-3 w-full shadow-md"
+    >
+      <h2 class="text-xl font-bold mb-2">
+        Text area
+      </h2>
+      <p class="mb-2">
+        Choose this text input as Textarea.
+      </p>
+      <div class="mb-2 flex">
+        <TTextInput placeholder="some placeholder" area />
+      </div>
+      <iframe
+        width="100%"
+        height="100"
+        src="//jsfiddle.net/mohstarsos1/56bxz90e/1/embedded/html/dark/"
         allowfullscreen="allowfullscreen"
         allowpaymentrequest
         frameborder="0"
