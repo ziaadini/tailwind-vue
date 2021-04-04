5db511cc9bcaf562360e3ab294f5100a0793f835
Amin Mokhtari <amin.mokhtari94@gmail.com>
2021-04-04 11:41:18 +0430

    update alert: use svg instead of icon

--- src/components/tailwind/alert/TAlert.vue
+++ src/components/tailwind/alert/TAlert.vue
@@ -43,18 +43,25 @@
         </div>
 
         <template v-if="!show">
-          <t-icon
+          <div
             @click="closeAlert"
             data-name="alert-close"
             :class="
-              renderClass(
-                'text-3xl cursor-pointer self-start leading-8',
-                'close'
-              )
+              renderClass(`cursor-pointer self-start fill-current `, 'close')
             "
             name="close"
           >
-          </t-icon>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="15"
+              height="15"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"
+              />
+            </svg>
+          </div>
         </template>
 
         <!--      </div>-->
