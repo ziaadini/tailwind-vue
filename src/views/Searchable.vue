c48fab939c5daed61c6dc1bbda0a8c5fba88c802
mohstarsos1 <mohammad.ghasemi68@gmail.com>
2021-04-28 15:00:47 +0430

    fix

--- src/views/Searchable.vue
+++ src/views/Searchable.vue
@@ -2,15 +2,18 @@
   <div class="flex flex-col items-start p-1" dir="ltr">
     <h3 class="text-5xl font-bold mb-2">Searchable</h3>
     <p class="mb-2">
-      Small filled buttons in different styles and colors can be used for call
-      to actions in forms and more.
+      With Searchable component, you can allow a user to search values that
+      present in a provided items list.
     </p>
 
     <div
       class="flex flex-col items-start mt-10 border rounded-sm p-3 w-full shadow-md"
     >
       <h2 class="text-xl font-bold mb-2">Simple</h2>
-      <p class="mb-2">Choose this basic button, only with text.</p>
+      <p class="mb-2">
+        With this type of component, only provide a list of data for search and
+        select an item via user.
+      </p>
       <div class="flex w-full items-center mb-4">
         <t-searchable
           class="w-1/6 mr-5"
@@ -40,12 +43,15 @@
       class="flex flex-col items-start mt-10 border rounded-sm p-3 w-full shadow-md"
     >
       <h2 class="text-xl font-bold mb-2">Custom No Result Message</h2>
-      <p class="mb-2">Choose this basic button, only with text.</p>
+      <p class="mb-2">
+        You can set a custom message to show user, when component not found
+        result to show.
+      </p>
       <div class="flex w-full items-center mb-4">
         <t-searchable
           noResult="Sorry, This is a test text!"
           class="w-1/6 mr-5"
-          v-model="searchableModel"
+          v-model="searchableModel2"
           placeholder="select..."
           :items="[
             { label: 'VueJS', value: 'VueJS' },
@@ -55,7 +61,7 @@
             { label: 'Angular', value: 'Angular' }
           ]"
         ></t-searchable>
-        <div>Selected item: {{ searchableModel }}</div>
+        <div>Selected item: {{ searchableModel2 }}</div>
       </div>
       <iframe
         width="100%"
@@ -71,22 +77,18 @@
       class="flex flex-col items-start mt-10 border rounded-sm p-3 w-full shadow-md"
     >
       <h2 class="text-xl font-bold mb-2">Delayed Search</h2>
-      <p class="mb-2">Choose this basic button, only with text.</p>
+      <p class="mb-2">This prop provide delay for debouncing.</p>
+      <span class="font-bold"
+        >notice: Only worked when localSearch prop is false.</span
+      ><br />
       <div class="flex w-full items-center mb-4">
         <t-searchable
           :delay="1000"
           class="w-1/6 mr-5"
-          v-model="searchableModel"
+          v-model="searchableModel3"
           placeholder="select..."
-          :items="[
-            { label: 'VueJS', value: 'VueJS' },
-            { label: 'NuxtJS', value: 'NuxtJS' },
-            { label: 'React', value: 'React' },
-            { label: 'Ember', value: 'Ember' },
-            { label: 'Angular', value: 'Angular' }
-          ]"
         ></t-searchable>
-        <div>Selected item: {{ searchableModel }}</div>
+        <div>Selected item: {{ searchableModel3 }}</div>
       </div>
       <iframe
         width="100%"
@@ -121,13 +123,29 @@ export default defineComponent({
   data() {
     return {
       mdFile: "",
-      searchableModel: ""
+      searchableModel: "",
+      searchableModel2: "",
+      searchableModel3: "",
+      searchableItems:[]
     };
   },
   async mounted() {
     this.mdFile = await fetch("/tailwind/searchable/README.md").then(res =>
       res.text()
     );
+  },
+  methods:{
+    searchAjax = ({ search, setLoading }) => {
+      setLoading(true);
+      fetch(
+        `https://api.github.com/search/repositories?q=${escape(search)}`
+      ).then(res => {
+        res.json().then(json => {
+          this.searchableItems.value = json.items;
+          setLoading(false);
+        });
+      });
+    };
   }
 });
 </script>
