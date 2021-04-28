5ccfbcd43db794fa4a6cb25897ce895295ae108e
mohstarsos1 <mohammad.ghasemi68@gmail.com>
2021-04-28 17:39:02 +0430

    fix some nt

--- src/views/NumberTransformer.vue
+++ src/views/NumberTransformer.vue
@@ -11,10 +11,75 @@
     >
       <h2 class="text-xl font-bold mb-2">Simple</h2>
       <p class="mb-2">Choose this basic button, only with text.</p>
-      <t-number-transformer
-        :number="number"
-        :step="1005"
-      ></t-number-transformer>
+      <div class="flex items-center mb-3">
+        <t-number-transformer
+          :number="number"
+          class="mr-4 w-40"
+        ></t-number-transformer>
+        <t-button @click="number = 1000" variant="success" size="sm"
+          >Transform</t-button
+        ><t-button @click="number = 0" variant="warning" size="sm" class="ml-2"
+          >Reset</t-button
+        >
+      </div>
+
+      <iframe
+        width="100%"
+        height="100"
+        src="//jsfiddle.net/mohstarsos1/13xgw0ry/8/embedded/html/dark"
+        allowfullscreen="allowfullscreen"
+        allowpaymentrequest
+        frameborder="0"
+      ></iframe>
+    </div>
+
+    <div
+      class="flex flex-col items-start mt-10 border rounded-sm p-3 w-full shadow-md"
+    >
+      <h2 class="text-xl font-bold mb-2">Duration</h2>
+      <p class="mb-2">Choose this basic button, only with text.</p>
+      <div class="flex items-center mb-3">
+        <t-number-transformer
+          :number="number2"
+          class="mr-4 w-40"
+          :maxDuration="2000"
+        ></t-number-transformer>
+        <t-button @click="number2 = 1000" variant="success" size="sm"
+          >Transform</t-button
+        ><t-button @click="number2 = 0" variant="warning" size="sm" class="ml-2"
+          >Reset</t-button
+        >
+      </div>
+
+      <iframe
+        width="100%"
+        height="100"
+        src="//jsfiddle.net/mohstarsos1/13xgw0ry/8/embedded/html/dark"
+        allowfullscreen="allowfullscreen"
+        allowpaymentrequest
+        frameborder="0"
+      ></iframe>
+    </div>
+
+    <div
+      class="flex flex-col items-start mt-10 border rounded-sm p-3 w-full shadow-md"
+    >
+      <h2 class="text-xl font-bold mb-2">Step</h2>
+      <p class="mb-2">Choose this basic button, only with text.</p>
+      <div class="flex items-center mb-3">
+        <t-number-transformer
+          :step="100000"
+          :number="number3"
+          class="mr-4 w-40"
+          :maxDuration="2000"
+        ></t-number-transformer>
+        <t-button @click="number3 = 1000000" variant="success" size="sm"
+          >Transform</t-button
+        ><t-button @click="number3 = 0" variant="warning" size="sm" class="ml-2"
+          >Reset</t-button
+        >
+      </div>
+
       <iframe
         width="100%"
         height="100"
@@ -36,11 +101,13 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import TNumberTransformer from "@/components/tailwind/numberTransformer/TNumberTransformer.vue";
+import TButton from "@/components/tailwind/button/TButton.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    TNumberTransformer
+    TNumberTransformer,
+    TButton
   },
   setup() {
     return {};
@@ -48,7 +115,9 @@ export default defineComponent({
   data() {
     return {
       mdFile: "",
-      number: 1000
+      number: 0,
+      number2: 0,
+      number3: 0
     };
   },
   async mounted() {
